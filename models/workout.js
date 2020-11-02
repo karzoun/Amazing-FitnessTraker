72f8ec8a871f7eb14cd8ed57c19b188c728595bc
“karzoun” <karzoun_27@hotmail.com>
2020-11-02 10:53:59 -0800

    fixing

--- models/workout.js
+++ models/workout.js
@@ -4,36 +4,38 @@ const Collection = mongoose.Schema;
 //create class instance of mongoose collection
 //call its constructor function
 const workoutCollection = new Collection({
-    day: {
-        type: Date,
-        default: () => new Date(),
+  day: {
+    type: Date,
+    default: () => new Date(),
+  },
+  exercises: [
+    {
+      type: {
+        type: String,
+        required: "Please input exercise type!!!",
+      },
+      name: {
+        type: String,
+        required: "Please inout exercise name!!!",
+      },
+      duration: {
+        type: Number,
+        required: "Please inout exercise duration!!!",
+      },
+      weight: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+      distance: {
+        type: Number,
+      },
     },
-    exercises: [{
-        type: {
-            type: String,
-            required: "Please input exercise type!!!",
-        },
-        name: {
-            type: String,
-            required: "Please inout exercise name!!!",
-        },
-        duration: {
-            type: Number,
-            required: "Please inout exercise duration!!!",
-        },
-        weight: {
-            type: Number,
-        },
-        reps: {
-            type: Number,
-        },
-        sets: {
-            type: Number,
-        },
-        distance: {
-            type: Number,
-        },
-    }, ],
+  ],
 });
 
 const WorkoutCollection = mongoose.model("Workout", workoutCollection);
